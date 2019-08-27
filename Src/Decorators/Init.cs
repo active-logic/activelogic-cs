b6dd3cc39c2c65e5c121e6e81375a69fe4d58466
Tea <tea.desouza@gmail.com>
2019-08-27 19:08:31 +0800

    Remove bugged init dec apis

--- Src/Decorators/Init.cs
+++ Src/Decorators/Init.cs
@@ -4,81 +4,35 @@
 #endif
 
 using static Active.Core.status;
-using static Active.Core.AbstractDecorator;  // for LogData
-using Active.Core.Details;
 using InvOp = System.InvalidOperationException;
 using Tag   = System.Runtime.CompilerServices.CallerLineNumberAttribute;
+
 namespace Active.Core{
 public class Init : AbstractDecorator{
 
     static int uid; internal static int id => uid = ID(uid);
-    const string INCOMPLETE = "Incomplete 'with' pattern";
-  #if !AL_OPTIMIZE
-    internal static LogData logData;
-    protected object target;
-  #endif
-    //
-    internal static Init current;
-    status preconditionStatus;
+
+    static Init current;
     public bool passing = true;
 
-    public Gate? this[status x]{ get{
-        if(current != null) throw new InvOp(INCOMPLETE);
-        current = this;
-      #if !AL_OPTIMIZE
-        logData = new LogData(this, x.trace.scope, x.trace.reason);
-      #endif
-        if(x.complete){
-            passing = false;
-            return new Gate(this);
-        }else{
-            preconditionStatus = x;
-            return null;
-        }
+    public Init pass{ get{
+        if(current != null)
+        { current = null; throw new InvOp("Unclosed init detected"); }
+        current = this; return passing ? this : null;
     }}
 
-    public Gate? this[object x]{ get{
-        if(current != null) throw new InvOp(INCOMPLETE);
-        current = this;
-        if(x != null){
-          #if !AL_OPTIMIZE
-            logData = new LogData(this, $"{x}: <{x.GetType().Name}>", null);
-          #endif
-            passing = false;
-            return new Gate(this);
-        }else{
-          #if !AL_OPTIMIZE
-            logData = new LogData(this, "<null>", null);
-          #endif
-            preconditionStatus = fail();
-            return null;
-        }
-    }}
+    public Gate this[object x]
+    { get{ passing = false; return new Gate(this); }}
 
     override public action Reset(){ passing = true; return @void(); }
 
-    internal static status PreconditionStatus(){
-        var s = Init.current.preconditionStatus;
-        Init.current = null;
-        return s;
-    }
-
-    // ------------------------------------------------------------------------
-
-    public readonly struct Gate{
+    new public readonly struct Gate{
 
         readonly Init owner;
 
         public Gate(Init self) => owner = self;
 
-        public StatusRef this[status s] { get{
-          #if !AL_OPTIMIZE
-            owner.target = s.targetScope;
-          #endif
-            owner.passing = !s.running;
-            Init.current = null;
-            return new StatusRef(s);
-        }}
+        static Init Owner(Gate? self) => self?.owner ?? Init.current;
 
         public static status operator % (Gate? self, status s)
         { Owner(self).passing = !s.running; Init.current = null; return s; }
@@ -89,45 +43,14 @@ public class Init : AbstractDecorator{
         public static status operator - (Gate? self, status s)
         { Owner(self).passing = s.failing; Init.current = null; return s; }
 
-        static Init Owner(Gate? self) => self?.owner ?? Init.current;
-
     }  // Gate class
 
-    public readonly struct StatusRef{
-
-        readonly status x;
-
-        public StatusRef(status value) => x = value;
-
-      #if AL_OPTIMIZE
-        public static implicit operator status(StatusRef? self)
-        => ToStatus(self);
-      #else
-        public static implicit operator status(StatusRef? self)
-        => status.log ? ToStatusWithLog(self) : ToStatus(self);
-      #endif
-
-        static status ToStatus(StatusRef? self)
-        => self?.x ?? Init.PreconditionStatus();
-
-      #if !AL_OPTIMIZE
-        static status ToStatusWithLog(StatusRef? self){
-            if(logData.scope == null) throw new InvOp("Log data is null");
-            return (self?.x ?? Init.PreconditionStatus())
-                   .ViaDecorator(logData.scope, log && logData.Reason());
-       }
-      #endif
-
-   }  // StatusRef class
-
 }
-
-// ----------------------------------------------------------------------------
 
 #if !AL_BEST_PERF
 partial class Task{
     public Init With([Tag] int key = -1)
-    => store.Decorator<Init>(key, Active.Core.Init.id);
+    => store.Decorator<Init>(key, Active.Core.Init.id).pass;
 }
 #endif
 
--- Tests/TestInit.cs
+++ Tests/TestInit.cs
@@ -1,95 +1,70 @@
 using System;
 using NUnit.Framework;
 using Active.Core;
-using NullRef = System.NullReferenceException;
 
 using static Active.Core.status;
 
-public class TestInit : DecoratorTest<Init> {
+public class TestInit : CoreTest {
 
-    override public void Setup(){
-        base.Setup();
-        // Need to reset this because testing purposely breaks the decorator
-        Init.current = null;
-    }
+    Init init;
 
-    [Test] public void TestInitFailingViaNull(){
-        string str = "peek";
-        status s = x?[ str = null ]?[ Substr(ref str, 1)];
-        o( str, null);
-    }
-
-    [Test] public void TestIgnoreFailingInitViaNull(){
-        string str = "peek";
-        Assert.Throws<NullRef>(
-            () => { status s = x?[ str = null ] + Substr(ref str, 1); }
-        );
-    }
+    [SetUp] public void Setup() => init = new Init();
 
     [Test] public void TestFirstPass(){
         string str = "";
-        status s = x?[ str = "boo" ] + Substr(ref str, 1);
+        status x = init.pass?[ str = "boo" ] + Substr(ref str, 1);
         o( str, "oo");
     }
 
     [Test] public void TestSecondPass(){
         string str = "peek";
-        status s = x?[ str = "boo" ] + Substr(ref str, 1);
-        o( s.complete );
-        o( str, "oo");
-        o( x.passing, true);
-        s = x?[ str = "boo" ] + Substr(ref str, 1);
-        o( s.complete );
+        init.passing = false;
+        status x = init.pass?[ str = "boo" ] + Substr(ref str, 1);
+        o( x.complete );
+        o( str, "eek");
+        o( init.passing, true);
+        x = init.pass?[ str = "boo" ] + Substr(ref str, 1);
+        o( x.complete );
         o( str, "oo" );
     }
 
     [Test] public void TestSecondPassWithPlusOp(){
-        x.passing = false;
-        status s;
-        s = x?[ noop ] + fail(); o( !x.passing );
-        s = x?[ noop ] + done(); o(  x.passing );
-        s = x?[ noop ] + cont(); o( !x.passing );
+        init.passing = false;
+        status x;
+        x = init.pass?[ noop ] + fail(); o( !init.passing );
+        x = init.pass?[ noop ] + done(); o(  init.passing );
+        x = init.pass?[ noop ] + cont(); o( !init.passing );
     }
 
     [Test] public void TestSecondPassWithMinusOp(){
-        x.passing = false;
-        status s;
-        s = x?[ noop ] - fail(); o(  x.passing );
-        s = x?[ noop ] - done(); o( !x.passing );
-        s = x?[ noop ] - cont(); o( !x.passing );
+        init.passing = false;
+        status x;
+        x = init.pass?[ noop ] - fail(); o(  init.passing );
+        x = init.pass?[ noop ] - done(); o( !init.passing );
+        x = init.pass?[ noop ] - cont(); o( !init.passing );
     }
 
     [Test] public void TestSecondPassWithNeutralOp(){
-        x.passing = false;
-        status s;
-        s = x?[ noop ] % fail(); o(  x.passing );
-        s = x?[ noop ] % done(); o(  x.passing );
-        s = x?[ noop ] % cont(); o( !x.passing );
+        init.passing = false;
+        status x;
+        x = init.pass?[ noop ] % fail(); o(  init.passing );
+        x = init.pass?[ noop ] % done(); o(  init.passing );
+        x = init.pass?[ noop ] % cont(); o( !init.passing );
     }
 
     [Test] public void TestDetectUnclosedInitExp(){
         string str = "";
-        Init.Gate? gate = x?[ str = "boo" ];
+        Init.Gate? x = init.pass?[ str = "boo" ];
         Assert.Throws<InvalidOperationException>( () => {
-            var s = x?[ str = "boo" ] + Substr(ref str, 1);
+            var s = init.pass?[ str = "boo" ] + Substr(ref str, 1);
         });
     }
 
-    [Test] public void StatusExp_InitFailing([Range(-1, +1)] int val)
-    { o( (status)x?[ fail() ]?[ ForInt(val) ], status._fail ); }
-
-    [Test] public void StatusExp_InitSucceeding([Range(-1, +1)] int val)
-    { o( (status)x?[ done() ]?[ ForInt(val) ], ForInt(val) ); }
-
-    [Test] public void StatusExp_InitRunning([Range(-1, +1)] int val)
-    { o( (status)x?[ cont() ]?[ ForInt(val) ], status._cont ); }
-
     object noop => 0;
-
-    status ForInt(int val) => status.@unchecked(val);
 
     status Substr(ref string s, int i){
         s = s.Substring(i);
+        //print();
         return status.done();
     }
 
