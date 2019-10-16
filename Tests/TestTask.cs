2163bad13463ea848fd004f3fd46494bc0062d1b
Tea <tea.desouza@gmail.com>
2019-10-16 23:26:59 +0800

    Disable undef test

--- Tests/TestTask.cs
+++ Tests/TestTask.cs
@@ -18,9 +18,8 @@ public class TestTaskUnit : CoreTest{
     [Test] public void Init()     => o( x.With(0)       != null);
     [Test] public void Latch()    => o( x.Latch(false)  == null);
     [Test] public void Once()     => o( x.Once(0)       != null);
-    #if (DEBUG || !AL_OPTIMIZE)
-    [Test] public void Undef()    => o( x.undef(0)      != null);
-    #endif
+    // TODO - enable in a branch first
+    //[Test] public void Undef()    => o( x.undef(0)      != null);
 
     class C : Task{}
 
