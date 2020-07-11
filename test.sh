3cdac5a7be759698b456cb08aa979bde2a52f114
Tea <tea@1k1.io>
2020-07-12 00:30:07 +0800

    Autoinstall reportgen

--- test.sh
+++ test.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 dotnet add "Tests/Tests.csproj" package coverlet.msbuild
 dotnet test -c Debug -p:CollectCoverage=true \
                      -p:CoverletOutputFormat=opencover
+# Install reportgen if needed
+dotnet tool update -g dotnet-reportgenerator-globaltool
+# Now run it
 reportgenerator -reports:Tests/coverage.opencover.xml \
                 -targetdir:CoverageReport
 open CoverageReport/index.htm
